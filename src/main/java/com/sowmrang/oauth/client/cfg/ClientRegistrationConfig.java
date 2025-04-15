806647ecbd94aedb6d7c7853f44143495d6b170f
sranganathan <sowmyanarayanan.ranganathan@amadeus.com>
2025-04-15 12:14:35 +0530

    changed to use java record

--- src/main/java/com/sowmrang/oauth/client/cfg/ClientRegistrationConfig.java
+++ src/main/java/com/sowmrang/oauth/client/cfg/ClientRegistrationConfig.java
@@ -1,16 +1,8 @@
 package com.sowmrang.oauth.client.cfg;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.PropertyMapper;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
-
-import java.security.Provider;
-import java.util.*;
 
 
 @ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.keycloak")
